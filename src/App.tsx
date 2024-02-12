8446cd6222aa31666bf2e623339145711096d2e8
Mohit Joshi <withmohitjoshi@gmail.com>
2024-02-12 20:14:17 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-function App() {
-  return <>Hello</>;
-}
+import { Header, Sidebar } from "./Components";
 
+function App() {
+  return <div id="app">
+    <Header />
+    <Sidebar />
+  </div>;
+}
 export default App;
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import { useIcons } from "../../Hooks/useIcons";
 
-const Header = () => {
+export default function Header() {
+  const { PrevBoardIcon, LastBoardIcon, NextBoardIcon, EndBoardIcon } =
+    useIcons();
   return (
-    <div>
-        
+    <div className="flex items-center gap-4">
+      <div id={"brand"} className="text-slate-600 text-2xl capitalize inline-block">
+        <p>white boardy</p>
+      </div>
+      <div className="flex items-center gap-2">
+        <span>
+          <LastBoardIcon />
+        </span>
+        <span>
+          <PrevBoardIcon />
+        </span>
+        <span className="text-slate-600">
+          Board{" "}
+          <input
+            inputMode="numeric"
+            type="number"
+            className="max-w-16 rounded-sm"
+            min={1}
+          />{" "}
+          Of 1
+        </span>
+        <span>
+          <NextBoardIcon />
+        </span>
+        <span>
+          <EndBoardIcon />
+        </span>
+      </div>
     </div>
-  )
+  );
 }
-
-export default Header
