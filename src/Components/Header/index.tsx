ddfde384ca202d6e3af7d3951eb41e6a61663d44
Mohit <mohit.joshi@capanicus.com>
2024-02-22 12:04:27 +0530

    changes

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,27 +1,19 @@
-import { useIcons } from "../../Hooks/useIcons";
+import { useIcons } from '../../Hooks/useIcons';
 
 export default function Header() {
-  const { FirstBoardIcon, PrevBoardIcon, NextBoardIcon, LastBoardIcon } =
-    useIcons();
+  const { FirstBoardIcon, PrevBoardIcon, NextBoardIcon, LastBoardIcon } = useIcons();
   return (
-    <header id="header">
-      <div id="brand">white boardy</div>
-      <div className="flex items-center gap-2 text-gray-700  [&>span]:cursor-pointer">
+    <header id='header'>
+      <div id='brand'>white boardy</div>
+      <div className='flex items-center gap-2 text-gray-700  [&>span]:cursor-pointer'>
         <span>
           <FirstBoardIcon />
         </span>
         <span>
           <PrevBoardIcon />
         </span>
-        <div className="border-x-[1px] border-gray-300 px-4">
-          Board{" "}
-          <input
-            type="number"
-            className="max-w-16 text-center rounded-s-sm"
-            min={1}
-            defaultValue={1}
-          />{" "}
-          of 1
+        <div className='border-x-[1px] border-gray-300 px-4'>
+          Board <input type='number' className='max-w-16 text-center rounded-s-sm' min={1} defaultValue={1} /> of 1
         </div>
         <span>
           <NextBoardIcon />
--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -1,56 +1,43 @@
-import { useIcons } from "../../Hooks/useIcons";
+import { useIcons } from '../../Hooks/useIcons';
 
 const Sidebar = () => {
-  const {
-    CursorIcon,
-    BoardLayerIcon,
-    ColorSelectIcon,
-    BoardColorSelectIcon,
-    RubberIcon,
-    ShapesIcon,
-    TextIcon,
-    ImageIcon,
-    TrashIcon,
-    RedoIcon,
-    UndoIcon,
-    PencilIcon,
-  } = useIcons();
+  const { CursorIcon, BoardLayerIcon, ColorSelectIcon, BoardColorSelectIcon, RubberIcon, ShapesIcon, TextIcon, ImageIcon, TrashIcon, RedoIcon, UndoIcon, PencilIcon } = useIcons();
   return (
-    <aside id="sidebar">
-      <span title="Cursor">
+    <aside id='sidebar'>
+      <span title='Cursor'>
         <CursorIcon />
       </span>
-      <span title="New Board">
+      <span title='New Board'>
         <BoardLayerIcon />
       </span>
-      <span title="Select pencil color">
+      <span title='Select pencil color'>
         <ColorSelectIcon />
       </span>
-      <span title="Select board color">
+      <span title='Select board color'>
         <BoardColorSelectIcon />
       </span>
-      <span title="Pencil">
+      <span title='Pencil'>
         <PencilIcon />
       </span>
-      <span title="Rbber">
+      <span title='Rubber'>
         <RubberIcon />
       </span>
-      <span title="Shapes">
+      <span title='Shapes'>
         <ShapesIcon />
       </span>
-      <span title="Text">
+      <span title='Text'>
         <TextIcon />
       </span>
-      <span title="Upload Image">
+      <span title='Upload Image'>
         <ImageIcon />
       </span>
-      <span title="Undo">
+      <span title='Undo'>
         <UndoIcon />
       </span>
-      <span title="Redo">
+      <span title='Redo'>
         <RedoIcon />
       </span>
-      <span title="Clear Board">
+      <span title='Clear Board'>
         <TrashIcon />
       </span>
     </aside>
