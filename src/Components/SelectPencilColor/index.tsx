9ac2f3af66819e5cd352069c4db09e79680ed65b
Mohit Joshi <withmohitjoshi@gmail.com>
2024-03-30 14:13:22 +0530

    changes

---
+++ src/Components/SelectPencilColor/index.tsx
@@ -0,0 +1,28 @@
+
+const colors = ["#ff0000", "#ffffff", "#000000", "#008000", "#ffff00"];
+
+const SelectPencilColor = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      {colors.map((color, index) => {
+        return (
+          <div
+            key={index}
+            className="rounded-md w-6 h-6"
+            style={{
+              backgroundColor: color,
+            }}
+          />
+        );
+      })}
+      <div>
+        <input
+          type="color"
+          className="w-6 h-6 p-1"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SelectPencilColor;
